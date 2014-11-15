e5b770671fa75246b74910f17b431d26ab86a597
Ashish Ranjan <hi.ashish26@gmail.com>
2014-11-15 18:29:50 +0000

    Added fig file to run graphite stack + Fixes in configs for the same

--- graphite-web/files/local_settings.py
+++ graphite-web/files/local_settings.py
@@ -46,7 +46,7 @@ LOG_METRIC_ACCESS = True
 # You should not use the loopback address (127.0.0.1) here if using clustering
 # as every webapp in the cluster should use the exact same values to prevent
 # unneeded cache misses. Set to [] to disable caching of images and fetched data
-MEMCACHE_HOSTS = ['127.0.0.1:11211']
+MEMCACHE_HOSTS = ['memcached:11211']
 #DEFAULT_CACHE_DURATION = 60 # Cache images and data for 1 minute
 
 
